13847aaf2a2849963a9db70129e6d61dc30768de
Bryan <dev.bryanc@gmail.com>
2024-07-16 19:40:06 +0800

    fix: theme default to light

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Loader from "./components/custom/Loader";
 function App() {
   return (
     <Suspense fallback={<Loader />}>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
         <BrowserRouter basename={ROUTE.BASE}>
           <RouteElements />
         </BrowserRouter>
